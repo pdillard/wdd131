872086bfea14987ca854e863b2b17816ba792e84
pdillard <pdillard@byui.edu>
2026-01-31 20:45:13 -0700

    Cool Pics Part 2

---
+++ coolpics/main.js
@@ -0,0 +1,36 @@
+const btn = document.querySelector('.menu');
+const menu = document.querySelector('nav');
+
+btn.addEventListener('click', () => {
+    menu.classList.toggle('show')
+});
+
+// Modal
+const gallery = document.querySelector('.gallery');
+const modal = document.querySelector('dialog');
+const modalImage = modal.querySelector('img');
+const closeButton = modal.querySelector('.close-viewer');
+
+gallery.addEventListener('click', openModal);
+
+function openModal(e) {
+    const img = e.target;
+    const src = img.getAttribute('src');
+    const alt = img.getAttribute('alt');
+    const full = src.replace('sm', 'full')
+
+    modalImage.src = full;
+    modalImage.alt = alt;
+
+    modal.showModal();
+}
+
+closeButton.addEventListener('click', () => {
+    modal.close();
+});
+
+modal.addEventListener('click', (event) => {
+    if (event.target == modal) {
+        modal.close();
+    }
+});
