4c2fbfc544497a39ca540b78ebf468f84a786c96
pdillard <pdillard@byui.edu>
2026-03-01 22:59:46 -0700

    charactercard

---
+++ charactercard/main.js
@@ -0,0 +1,42 @@
+const character = {
+    name: "Swamp Beast Diplomat",
+    class: "Diplomat",
+    level: 1,
+    health: 100,
+    image: "swamp-beast.jpg",
+
+    attacked() {
+        this.health -= 20;
+
+        if (this.health <= 0) {
+            this.health = 0;
+            updateCard();
+            alert(this.name + " has died.");
+            return;
+        }
+
+        updateCard();
+    },
+
+    levelUp() {
+        this.level += 1;
+        updateCard();
+    }
+};
+
+function updateCard() {
+    document.getElementById("name").textContent = character.name;
+    document.getElementById("class").textContent = "Class: " + character.class;
+    document.getElementById("level").textContent = "Level: " + character.level;
+    document.getElementById("health").textContent = "Health: " + character.health;
+}
+
+function attackCharacter() {
+    character.attacked();
+}
+
+function levelCharacter() {
+    character.levelUp();
+}
+
+updateCard();
